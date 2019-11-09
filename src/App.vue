b9e71fc6a94151c02165b63e2b6277298047d6f6
Daniel Roe <daniel@concision.co.uk>
2019-11-09 08:53:00 +0000

    feat: add typed store accessor

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,11 @@ import HelloWorld from './components/HelloWorld.vue';
     HelloWorld,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  mounted() {
+    this.$accessor.resetEmail();
+  }
+}
 </script>
 
 <style>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,51 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { useAccessor, mutationTree, actionTree } from 'nuxt-typed-vuex'
+
+import * as submodule from './submodule'
+
+export const state = () => ({
+  email: '',
+})
+
+type RootState = ReturnType<typeof state>
+
+export const getters = {
+  email: (state: RootState) => state.email,
+  fullEmail: (state: RootState) => state.email,
+}
+
+export const mutations = mutationTree(state, {
+  setEmail(state, newValue: string) {
+    state.email = newValue
+  },
+
+  initialiseStore() {
+    // console.log('initialised')
+  },
+})
+
+export const actions = actionTree(
+  { state, getters, mutations },
+  {
+    async resetEmail({ commit }) {
+      commit('setEmail', 'a@a.com')
+    },
+  }
+)
+
+export const storePattern = {
+  state,
+  mutations,
+  actions,
+  modules: { submodule },
+}
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+const store = new Vuex.Store(storePattern)
+export const accessor = useAccessor(store, storePattern)
+
+Vue.prototype.$accessor = accessor
+
+export default store
